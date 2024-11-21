428ae39cf150525c9fc098119b095ae69fa2190d
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-11-21 19:11:18 -0300

    Feat: Criado usuario model
    -Criado com intuito de servir como base de informações do usuario para a criação de contas

---
+++ Model/UsuarioModel.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_Erp.Model
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O nome deve ter entre 2 e 50 caracteres.")]
+        public string Nome { get; set;}
+
+        [Required(ErrorMessage = "O sobrenome é obrigatório.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O sobrenome deve ter entre 2 e 50 caracteres.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O endereço não pode exceder 100 caracteres.")]
+        
+        public string Endereco { get; set; }
+        
+        [Required(ErrorMessage = "O número de telefone é obrigatório.")]
+        [RegularExpression(@"^\d{10,11}$", ErrorMessage = "O número de telefone deve ter entre 10 E 11 dígitos.")]
+        public string NumeroTelefone { get; set; }
+        public DateTime CriacaoConta { get; set; } = DateTime.Now;
+
+        public byte[] PasswordHash {get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
