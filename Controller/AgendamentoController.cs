a7283cfc3b2b9c83346d63d0338fc4ee5702f163
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-12-13 23:08:23 -0300

    Feat: Criado AgendamentoController para gerenciamento de agendamentos
    -[GET] medicos/{especialidadeMedica}
    -[GET] DiasDisponiveis/{medicoId}
    -[POST] agendar

---
+++ Controller/AgendamentoController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System_Erp.Dto;
+using System_Erp.Enum;
+using System_Erp.Services;
+
+namespace System_Erp.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        private readonly IAgendamentoService _agendamento;
+
+        public AgendamentoController(IAgendamentoService agendamento)
+        {
+            _agendamento = agendamento;
+        }
+
+        [HttpGet("medicos/{especialidadeMedica}")]
+        public async Task<ActionResult<List<InfoMedicoDto>>> ObterMedicosPorCargo (EspecialidadeMedica especialidadeMedica)
+        {
+            var medicos = await _agendamento.ObterMedicosPorCargo(especialidadeMedica);
+            if (medicos == null || medicos.Count == 0)
+            {
+                return NotFound("Nenhum médico encontrado para para o cargo solicitado");
+            }
+            return Ok(medicos);
+        }
+
+        [HttpGet("DiasDisponiveis/{medicoId}")]
+        public async Task<ActionResult<List<DateTime>>> ObterDiasDisponiveis (int medicoId, int diasAntecipacao = 30)
+        {
+            var diasDisponiveis = await _agendamento.ObterDiasDisponiveis(medicoId, diasAntecipacao);
+            if (diasDisponiveis == null || diasDisponiveis.Count == 0)
+            {
+                return NotFound("Nenhum dia disponível encontrado para o médico especificado.");
+            } 
+            return Ok(diasDisponiveis);
+        }
+
+        [HttpPost("agendar")]
+        public async Task<ActionResult> AgendarConsulta (AgendamentoDto agendamentoDto)
+        {
+            if (agendamentoDto == null)
+            {
+                return BadRequest("Os dados do agendamento são inválidos.");
+            }
+
+            var resultado = await _agendamento.AgendarConsulta(agendamentoDto);
+            if (!resultado)
+            {
+                return BadRequest("Não foi possível agendar a consulta. Verifique se o horário está disponível ou se o intervalo de 40 minutos foi respeitado.");
+            }
+
+            return Ok("Consulta agendada com sucesso.");
+        }
+    }
+}
