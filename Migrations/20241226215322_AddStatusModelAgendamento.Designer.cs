75850520be1be2c006acffdfd64f1d1e7d7b7cd7
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-12-26 19:21:27 -0300

    Realização de migrations AddStatusModelAgendamento

--- Migrations/20241226215322_AddStatusModelAgendamento.Designer.cs
+++ Migrations/20241226215322_AddStatusModelAgendamento.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System_Erp.Data;
 
@@ -11,9 +12,11 @@ using System_Erp.Data;
 namespace System_Erp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241226215322_AddStatusModelAgendamento")]
+    partial class AddStatusModelAgendamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
